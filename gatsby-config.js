7b997f141b50beae637fa266558a6815990df176
Romain Mosser <mosser.romain@gmail.com>
2023-03-09 22:16:59 +0100

    Configured Strapi API with basic calls

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,27 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ["recipe"],
+  singleTypes: ["home-page"],
+};
+
 module.exports = {
   siteMetadata: {
     title: `RE7`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,58 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 
-const IndexPage = () => {
+export const query = graphql`
+  query {
+    allStrapiHomePage {
+      edges {
+        node {
+          hero_title
+        }
+      }
+    }
+    allStrapiRecipe {
+      edges {
+        node {
+          id
+          recipe_title
+          ingredient {
+            id
+            ingredient
+            quantity
+            unit
+          }
+          intro {
+            data {
+              internal {
+                content
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => {
+  const homePageContent = data.allStrapiHomePage.edges[0].node
+  const recipesData = data.allStrapiRecipe.edges
+  console.log(recipesData)
   return (
     <main>
-      <h1>Recipe App</h1>
+      <h1>{homePageContent.hero_title}</h1>
+      {recipesData.map((recipe) => (
+        <div key={recipe.node.id}>
+            <h1>{recipe.node.recipe_title}</h1>
+            <h2>Ingrédients :</h2>
+            <ul>
+              {recipe.node.ingredient.map((ingredient) => (
+                <li key={ingredient.id}>{ingredient.quantity} {ingredient.unit} de {ingredient.ingredient}</li>
+              ))}
+            </ul>
+            <p>{recipe.node.intro.data.internal.content.slice(0, 250)}...</p>
+        </div>
+      ))}
     </main>
   )
 }
